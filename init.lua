b5af18f25b492777991e58b7d5a5735ea0c2e9ce
Malin Elser <malin.elser@volvo.com>
2025-09-12 11:32:23 +0200

    fixed mini surround key mapping

--- init.lua
+++ init.lua
@@ -940,7 +940,17 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'gsa', -- Add surrounding
+          delete = 'gsd', -- Delete surrounding
+          find = 'gsf', -- Find right surrounding
+          find_left = 'gsF', -- Find left surrounding
+          highlight = 'gsh', -- Highlight surrounding
+          replace = 'gsr', -- Replace surrounding
+          update_n_lines = 'gsn',
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
--- lua/kickstart/plugins/leap.lua
+++ lua/kickstart/plugins/leap.lua
@@ -1,8 +1,17 @@
 -- https://github.com/ggandor/leap.nvim
-
 return {
   'ggandor/leap.nvim',
   config = function()
+    -- Apply default mappings first (optional if you want S/gs as well)
     require('leap').set_default_mappings()
+
+    -- Override `s` in normal/visual/operator-pending modes
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', function()
+      require('leap').leap {
+        target_windows = vim.tbl_filter(function(win)
+          return vim.api.nvim_win_get_config(win).relative == ''
+        end, vim.api.nvim_tabpage_list_wins(0)),
+      }
+    end, { desc = 'Leap across windows with s' })
   end,
 }
